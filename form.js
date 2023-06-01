13686e7581d8662b60e823c000a6c73e2993ef9c
El Mhamdi Khalil <63989059+KhalilElMhamdi@users.noreply.github.com>
2023-06-01 14:03:58 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+<script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/embed/v2.js"></script>
+
+  hbspt.forms.create({
+    region: "eu1",
+    portalId: "139673104",
+    formId: "c53c5634-1fe8-4dcd-9bb3-3a5d1c8d34e0"
+  });
